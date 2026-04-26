5f408a2f219deaffb84906ab050c68da6305e157
ahmadsaeedmirza <ahmadsaeedas023@gmail.com>
2026-04-26 13:53:41 +0500

    integrated google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
 import Index from "./pages/Index.tsx";
 import Services from "./pages/Services.tsx";
 import Work from "./pages/Work.tsx";
@@ -13,12 +16,22 @@ import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // This will now work because BrowserRouter is in main.tsx
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search
+    });
+  }, [location]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
@@ -26,12 +39,11 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/process" element={<Process />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import this
 import App from "./App.tsx";
 import "./index.css";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Initialize GA4
+ReactGA.initialize("G-01TG7PFKVG");
+
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
